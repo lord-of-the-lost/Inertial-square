8955221f86e506b0d64f90e20a6458c8a33fbd2e
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-07-18 14:16:47 +0300

    Initial Commit

---
+++ Inertial square/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inertial square
+//
+//  Created by Николай Игнатов on 18.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
